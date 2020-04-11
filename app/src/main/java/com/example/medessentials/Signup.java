40fc84f089bcb70a258eadf5d801f3a29220258a
George Shao <GeorgeShao123@gmail.com>
2020-04-11 18:52:21 -0400

    Added string array and adapted it to a Spinner/Dropdown.

--- app/src/main/java/com/example/medessentials/Signup.java
+++ app/src/main/java/com/example/medessentials/Signup.java
@@ -2,6 +2,8 @@ package com.example.medessentials;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Signup extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class Signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        String[] arraySpinner = new String[] {
+                "Individual Donor", "Manufacturer Donor", "Medical Professional (Recipient)"
+        };
+        Spinner s = (Spinner) findViewById(R.id.persontype_dropdown);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arraySpinner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
     }
 }
