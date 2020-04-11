03d274e347d2e597f05a0b80a3e2d367a98f4236
George Shao <GeorgeShao123@gmail.com>
2020-04-11 19:44:24 -0400

    Created Offer Activity and Java Class

---
+++ app/src/main/java/com/example/medessentials/Offer.java
@@ -0,0 +1,15 @@
+package com.example.medessentials;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Offer extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offer);
+    }
+}
