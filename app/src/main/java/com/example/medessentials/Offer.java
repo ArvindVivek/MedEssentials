8f0286cf1f1d51b924ac97f6ef9f908bfb076d3b
George Shao <GeorgeShao123@gmail.com>
2020-04-11 21:09:46 -0400

    Resolved missing imports

--- app/src/main/java/com/example/medessentials/Offer.java
+++ app/src/main/java/com/example/medessentials/Offer.java
@@ -1,7 +1,15 @@
 package com.example.medessentials;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Offer extends AppCompatActivity {
