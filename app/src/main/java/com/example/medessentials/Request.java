9992f1192c3be961b0ee820f96b35a915ecffbca
George Shao <GeorgeShao123@gmail.com>
2020-04-11 21:04:15 -0400

    set content view to the Request xml layout

--- app/src/main/java/com/example/medessentials/Request.java
+++ app/src/main/java/com/example/medessentials/Request.java
@@ -8,5 +8,6 @@ public class Request extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request);
     }
 }
