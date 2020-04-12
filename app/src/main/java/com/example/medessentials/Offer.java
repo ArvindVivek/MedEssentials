0187cbffabb3f284b94ce9f9eb7b3026e9f336ab
Justin Yue <flyingjay16@gmail.com>
2020-04-11 17:53:54 -0700

    Logout functionality added

--- app/src/main/java/com/example/medessentials/Offer.java
+++ app/src/main/java/com/example/medessentials/Offer.java
@@ -1,15 +1,44 @@
 package com.example.medessentials;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class Offer extends AppCompatActivity {
+
+    Button logout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offer);
+
+        logout = findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logout();
+            }
+        });
+    }
+
+    private void logout() {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Intent intent = new Intent(getApplicationContext(), Authentication.class);
+                        startActivity(intent);
+                    }
+                });
     }
 }
